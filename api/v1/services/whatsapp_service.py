4511cf544c4492a44445023b1de1a2d7ffbec693
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2026-06-18 12:10:47 +0100

    fix: update WhatsApp notification template and restore BASE_URL for future use

--- api/v1/services/whatsapp_service.py
+++ api/v1/services/whatsapp_service.py
@@ -1,9 +1,6 @@
 import httpx
-import os
 from api.utils.config import settings
 
-
-BASE_URL = f"https://graph.facebook.com/v25.0/{settings.WHATSAPP_PHONE_NUMBER_ID}/messages"
 
 HEADERS = {
     "Authorization": f"Bearer {settings.WHATSAPP_ACCESS_TOKEN}",
@@ -14,31 +11,26 @@ async def send_invite_notification(to: str, guest_name: str, event_name: str, in
     """Send an invite notification via WhatsApp"""
     payload = {
         "messaging_product": "whatsapp",
-        "to": to,  # format: 2348XXXXXXXXX
+        "to": to,  # format: 2348XXXXXXXXX or +2347XXXXXXXXXXX
         "type": "template",
         "template": {
-            "name": "hello_world",
-            # "name": f"hello, {guest_name}, you're invited to {event_name}! Click here to RSVP: {invite_link}",  # replace with your custom template later
-            "language": {"code": "en_US"},
-        }
-    #     "template": {
-    #     "name": "invite_notification",
-    #     "language": {"code": "en"},
-    #     "components": [
-    #         {
-    #             "type": "body",
-    #             "parameters": [
-    #                 {"type": "text", "text": guest_name},
-    #                 {"type": "text", "text": event_name},
-    #                 {"type": "text", "text": invite_link}
-    #             ]
-    #         }
-    #     ]
-    # }
+        "name": "invite_notification",
+        "language": {"code": "en"},
+        "components": [
+            {
+                "type": "body",
+                "parameters": [
+                    {"type": "text", "text": guest_name},
+                    {"type": "text", "text": event_name},
+                    {"type": "link", "text": invite_link}
+                ]
+            }
+        ]
+    }
     }
 
     async with httpx.AsyncClient() as client:
-        response = await client.post(BASE_URL, headers=HEADERS, json=payload)
+        response = await client.post(settings.WHATSAPP_BASE_URL, headers=HEADERS, json=payload)
         print("Status:", response.status_code)
         print("Response:", response.text)
         response.raise_for_status()
